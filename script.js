d2585abb0184dcc08b9dfac540911080e2d27f4d
ViniQF <144246843+ViniQF@users.noreply.github.com>
2023-09-15 08:10:34 -0300

    Projeto Basico

---
+++ script.js
@@ -0,0 +1,70 @@
+function Kyan(){
+    const myJSON_end = '["Nome: Renan Mesquita da Silva ", "Idade: 25 Anos ", "Nasceu em: Praia Grande ", "Stream no Spotify: 3.900.000"]';
+    console.log(myJSON_end)
+
+    const myArray_end= JSON.parse(myJSON_end)
+    console.log(myArray_end)
+
+    document.getElementById("artistas").innerHTML=
+        "<p><img src='https://pbs.twimg.com/media/EZiEo3bWkAAh_Mo?format=jpg&name=large' alt='40' widht='700' height='600'>" + 
+        "<p>"+myArray_end[0]+"<\p>"+
+        "<p>"+myArray_end[1]+"<\p>"+
+        "<p>"+myArray_end[2]+"<\p>"+
+        "<p>"+myArray_end[3]+"<\p>" 
+}
+function Derek(){
+    const myJSON_end = '["Nome: Derek Luccas Da Silva ", "Idade: 26 Anos ", "Nasceu em: São Paulo-Vila Prudente ", "Stream no Spotify: 1.605.819"]';
+    console.log(myJSON_end)
+
+    const myArray_end= JSON.parse(myJSON_end)
+    console.log(myArray_end)
+
+    document.getElementById("artistas").innerHTML=
+        "<p><img src='https://rollingstone.uol.com.br/media/uploads/derek_2_recayd_mob_credito_jef_delgado.jpg'  alt='40' widht='700' height='600'>" + 
+        "<p>"+myArray_end[0]+"<\p>"+
+        "<p>"+myArray_end[1]+"<\p>"+
+        "<p>"+myArray_end[2]+"<\p>"+
+        "<p>"+myArray_end[3]+"<\p>" 
+}
+function Ig(){
+    const myJSON_end = '["Nome: Igor Guilherme ", "Idade: 26 Anos ", "Nasceu em: São Paulo-Vila Medeiros ", "Stream no Spotify: 5.942.144"]';
+    console.log(myJSON_end)
+
+    const myArray_end= JSON.parse(myJSON_end)
+    console.log(myArray_end)
+
+    document.getElementById("artistas").innerHTML=
+        "<p><img src='https://i1.sndcdn.com/artworks-3N6QSM3zFOkWfFHg-4hY96Q-t500x500.jpg'  alt='40' widht='600' height='500'>" + 
+        "<p>"+myArray_end[0]+"<\p>"+
+        "<p>"+myArray_end[1]+"<\p>"+
+        "<p>"+myArray_end[2]+"<\p>"+
+        "<p>"+myArray_end[3]+"<\p>" 
+}
+function Mano(){
+    const myJSON_end = '["Nome: Pedro Paulo Soares Pereira ", "Idade: 53 Anos ", "Nasceu em: São Paulo-Capão Redondo ", "Stream no Spotify: 3.286.523"]';
+    console.log(myJSON_end)
+
+    const myArray_end= JSON.parse(myJSON_end)
+    console.log(myArray_end)
+
+    document.getElementById("artistas").innerHTML=
+        "<p><img src='https://f.i.uol.com.br/fotografia/2021/12/01/163837788361a7a99b5d678_1638377883_3x2_md.jpg'  alt='40' widht='700' height='600'>" + 
+        "<p>"+myArray_end[0]+"<\p>"+
+        "<p>"+myArray_end[1]+"<\p>"+
+        "<p>"+myArray_end[2]+"<\p>"+
+        "<p>"+myArray_end[3]+"<\p>" 
+}
+function Cent(){
+    const myJSON_end = '["Nome: Curtis James Jackson III ", "Idade: 48 Anos ", "Nasceu em:  Nova Iorque ", "Stream no Spotify: 31.442.451"]';
+    console.log(myJSON_end)
+
+    const myArray_end= JSON.parse(myJSON_end)
+    console.log(myArray_end)
+
+    document.getElementById("artistas").innerHTML=
+        "<p><img src='https://i.scdn.co/image/dd031b9c5d1b6eba4a691cd89c954255aae787f2'  alt='40' widht='700' height='600'>" + 
+        "<p>"+myArray_end[0]+"<\p>"+
+        "<p>"+myArray_end[1]+"<\p>"+
+        "<p>"+myArray_end[2]+"<\p>"+
+        "<p>"+myArray_end[3]+"<\p>" 
+}
